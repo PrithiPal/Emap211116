2d45ef148b47cf714e396f6269f84ad8424215b4
Akshay <akshayn1388@gmail.com>
2016-11-17 03:58:00 +0000

    Akshay: Supervisor Logiin and Sign_up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117030105) do
+ActiveRecord::Schema.define(version: 20161117035309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,24 @@ ActiveRecord::Schema.define(version: 20161117030105) do
 
   add_index "guards", ["email"], name: "index_guards_on_email", unique: true, using: :btree
   add_index "guards", ["reset_password_token"], name: "index_guards_on_reset_password_token", unique: true, using: :btree
+
+  create_table "supervisors", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "supervisors", ["email"], name: "index_supervisors_on_email", unique: true, using: :btree
+  add_index "supervisors", ["reset_password_token"], name: "index_supervisors_on_reset_password_token", unique: true, using: :btree
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
