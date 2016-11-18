c042026cec7d2ae7f3bfa08e5d4f1e63d6154f0a
PrithiPal <pprithip@sfu.ca>
2016-11-18 13:26:34 +0000

    After sign_in/out link understood

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :supervisors
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :guards, controllers: {sessions: 'users/sessions'}
+  devise_for :guards
   resources :widgets
   resources :guards
   resources :supervisors
