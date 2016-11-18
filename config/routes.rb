7ad6f56483fa8ac7eb2bffe21c4a4b49a1015b97
PrithiPal <pprithip@sfu.ca>
2016-11-18 12:12:17 +0000

    Guards session not working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :supervisors
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :guards, controllers: {sessions: 'users/sessions'}
+  devise_for :guards, controllers: {sessions: 'guard/sessions'}
   resources :widgets
   resources :guards
   resources :supervisors
