5ef5ca48994cf6ec81fd319ca67c28975b8f8bdd
Rail <railneer@users.noreply.github.com>
2016-11-21 08:17:46 +0530

    Signed-off-by: Rail <railneer@users.noreply.github.com>

--- app/models/guard.rb
+++ app/models/guard.rb
@@ -6,5 +6,26 @@ class Guard < ActiveRecord::Base
   belongs_to :supervisor
 
   geocoded_by :current_sign_in_ip
-  after_validation :geocode
+  before_validation :update_lat_long
+
+  def get_weather_info
+    return unless latitude && longitude
+    url = URI.parse("http://api.openweathermap.org/data/2.5/weather?lat=#{latitude}&lon=#{longitude}&APPID=#{Rails.application.secrets.weather_api_key}")
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    res.body
+  end
+
+  def update_lat_long
+    coordinates = geocode
+    if geocode
+      self.latitude = coordinates.first
+      self.longitude = coordinates.last
+    else
+      self.latitude = nil
+      self.longitude = nil
+    end
+  end
 end
